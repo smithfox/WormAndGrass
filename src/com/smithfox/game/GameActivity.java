3c7e5495e53c17c0c735dc39e563e2b26a3b90a3
smithfox <ssm.fox@gmail.com>
2012-07-24 22:14:35 +0800

    改一种方式,测试还有些问题

--- src/com/smithfox/game/GameActivity.java
+++ src/com/smithfox/game/GameActivity.java
@@ -4,17 +4,61 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
 
 public class GameActivity extends Activity {
 	private final static String TAG="GA";
 	private final static Handler handler = new Handler();
 	
+	private SurfaceView gameview;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-	}
 
+        gameview=(SurfaceView)this.findViewById(R.id.gameview);
+
+//        gameview.setLongClickable(true);
+//        gameview.setKeepScreenOn(true);// 保持屏幕常亮
+
+        //gameview.getHolder().setFixedSize(300, 300);//设置分辨率
+        gameview.getHolder().setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);//设置surfaceview不维护自己的缓冲区，而是等待屏幕的渲染引擎将内容推送到用户面前
+        gameview.getHolder().addCallback(new GameSurface(gameview.getHolder()));//对surface对象的状态进行监听
+        
+        ButtonOnClikListiner buttonOnClikListinero=new ButtonOnClikListiner();
+        Button btn1=(Button) this.findViewById(R.id.button1);
+        Button btn2=(Button) this.findViewById(R.id.button2);
+        Button btn3=(Button) this.findViewById(R.id.button3);
+        btn1.setOnClickListener(buttonOnClikListinero);
+        btn2.setOnClickListener(buttonOnClikListinero);
+        btn3.setOnClickListener(buttonOnClikListinero);
+	}
+	
+	private final class ButtonOnClikListiner implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+//            if(Environment.getExternalStorageState()==Environment.MEDIA_UNMOUNTED){
+//                Toast.makeText(GameActivity.this, "sd卡不存在", Toast.LENGTH_SHORT).show();
+//                return;
+//            }
+
+            switch (v.getId()) {
+            case R.id.button1:
+                //
+                break;
+            case R.id.button2:
+                //
+                break;
+            case R.id.button3:
+                //
+                break;
+            }
+        }
+	}
+	
 	@Override
 	protected void onDestroy() {
 		Log.d(TAG, "onDestroy");
---
+++ src/com/smithfox/game/GameSurface.java
@@ -0,0 +1,88 @@
+package com.smithfox.game;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.Log;
+import android.view.SurfaceHolder;
+
+public class GameSurface implements SurfaceHolder.Callback, Runnable {
+	private final static String TAG="GS";
+	private Thread myThread;
+	
+	private SurfaceHolder holder;
+	private int ScreenW=0;
+	private int ScreenH=0;
+	public boolean isRun;
+
+	public GameSurface(SurfaceHolder holder) {
+		this.holder = holder;
+//		ScreenW = w;
+//        ScreenH = h;
+	}
+	
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		Log.d(TAG, "surfaceCreated");
+		this.isRun = true;
+		myThread = new Thread(this);
+		myThread.start();
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,	int height) {
+		Log.d(TAG, "surfaceChanged, w="+width+",h="+height);
+		this.ScreenH = height;
+		this.ScreenW = width;
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		Log.d(TAG, "surfaceDestroyed");
+		this.isRun = false;
+	}
+	
+	private void draw(int count) {
+		Canvas c = null;
+		try {
+			Log.d(TAG, "draw");
+			c = holder.lockCanvas();
+			if(c!=null) {
+				//c.drawColor(Color.BLACK);
+				Paint p = new Paint();
+				p.setColor(Color.WHITE);
+				Rect r = new Rect(50, 50, ScreenW-50, ScreenH-50);
+				//Rect r = new Rect(50, 50, 100, 100);
+				c.drawRect(r, p);
+				p.setColor(Color.BLUE);
+				c.drawText("现在:" + count + "秒", 100, 310, p);
+			} else {
+				Log.d(TAG, "lockCanvas return null");
+			}
+		} catch (Exception e) {
+			Log.e(TAG, "", e);
+		} finally {
+			if (c != null) {
+				holder.unlockCanvasAndPost(c);
+			}
+		}
+	}
+
+	@Override
+	public void run() {
+		int count = 0;
+		Log.d(TAG, "Thread start");
+		while (isRun) {
+			draw(count);
+			count++;
+            try {
+                Thread.sleep(1000);
+            } catch (Exception ex) {
+            }
+		}
+
+		Log.d(TAG, "Thread end");
+	}
+	
+}
